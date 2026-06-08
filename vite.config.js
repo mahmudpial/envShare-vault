b3f65341082451cb42815065df84bcae38a1239c
pial <pialmahmud80@gmail.com>
2026-06-09 04:37:18 +0600

    update vercel.json

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
     plugins: [
         laravel({
-            input: "resources/js/app.js",
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
+            buildDirectory: "build", // Ensure this is 'build'
         }),
         vue({
             template: {
